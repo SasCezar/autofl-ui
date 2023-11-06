e61bedfb74e34355d71bb0f685a635422ff1ac53
Cezar Sas <cezar.sas@gmail.com>
2023-11-06 10:36:39 +0100

    WIP - Fix UI

--- src/request/file_annotation.py
+++ src/request/file_annotation.py
@@ -9,15 +9,15 @@ import streamlit as st
 def annotate_file(project_name: str, remote: str, languages: List[str]):
     url = 'http://auto-fl:8000/label/files'
     analysis = {
-        "name": project_name, #"Waikato|weka-3.8",
-        "remote": remote, # "https://github.com/Waikato/weka-3.8",
-        "languages": languages #["java"]
+        "name": project_name,  # "Waikato|weka-3.8",
+        "remote": remote,  # "https://github.com/Waikato/weka-3.8",
+        "languages": languages  # ["java"]
     }
 
     res = requests.post(url, json=analysis)
     res = res.json()['result']
     annotations_df = pd.DataFrame.from_dict(res['versions'][0]['files_annotation']).transpose()
-    package_map = res['versions'][0]['files_packages']
+    package_map = {file.path:file.package for file in res['versions'][0]['files'].values()}
     annotations_df['package'] = [package_map[x] for x in annotations_df.index]
     taxonomy = res['taxonomy']
     return annotations_df, taxonomy
